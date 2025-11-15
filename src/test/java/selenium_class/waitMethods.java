6494c2a4e192e024322eafb6e0b3281809c76c85
Yogeeswar Achari <yogeeswar203@gmail.com>
2025-11-15 23:46:05 +0530

    Commit 2 added wait command and GetWindow Handles

--- src/test/java/selenium_class/waitMethods.java
+++ src/test/java/selenium_class/waitMethods.java
@@ -3,6 +3,9 @@ package selenium_class;
 import java.sql.Time;
 import java.time.Duration;
 import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,18 +20,63 @@ public class waitMethods {
 	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver dr  = new ChromeDriver();
-		dr.get("https://omayo.blogspot.com/");
-		dr.manage().window().maximize();
-		Thread.sleep(1000);
+		/*
+		 * dr.get("https://omayo.blogspot.com/"); dr.manage().window().maximize();
+		 * Thread.sleep(1000);
+		 * dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		 * 
+		 * 
+		 * WebElement el = dr.findElement(By.xpath("//form[@name='form1']//input[1]"));
+		 * 
+		 * 
+		 * // Implicit wait
+		 * dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		 * 
+		 * // Explicit wait WebDriverWait myWait = new WebDriverWait(dr,
+		 * Duration.ofSeconds(15)); myWait.until(ExpectedConditions.visibilityOf(el));
+		 * 
+		 * el.sendKeys("test@gmal.com");
+		 * 
+		 * 
+		 * String winID = dr.getWindowHandle();
+		 * System.out.println("Current window ID is: "+winID);
+		 * 
+		 * WebElement windidEl =
+		 * dr.findElement(By.xpath("//a[text()='Open a popup window']"));
+		 * windidEl.click();
+		 * 
+		 * Set<String> set= dr.getWindowHandles(); List<String> set1 = new
+		 * ArrayList<String>(set); String p1 = set1.get(0); String p2 = set1.get(1);
+		 * 
+		 * dr.switchTo().window(p2);
+		 * 
+		 * String newWind =
+		 * dr.findElement(By.xpath("//h3[text()='New Window']")).getText();
+		 * System.out.println("New Window text is: "+newWind); dr.switchTo().window(p1);
+		 * for(String wl:set) { String title = dr.getTitle(); System.out.println(title);
+		 * }
+		 * 
+		 * Thread.sleep(5000); System.out.println("Driver is executed successfully");
+		 */
+		
 		dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		dr.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		WebElement e1 = dr.findElement(By.xpath("//a[text()='OrangeHRM, Inc']"));
 		
-		WebDriverWait myWait  = new WebDriverWait(dr, Duration.ofSeconds(10));
-		WebElement el  = dr.findElement(By.xpath("//form[@name='form1']//input[1]"));
-		myWait.until(ExpectedConditions.elementToBeClickable(el));
-		el.sendKeys("username1");
+		WebDriverWait mywait  = new WebDriverWait(dr, Duration.ofSeconds(10));
+		mywait.until(ExpectedConditions.visibilityOf(e1));
+		e1.click();
 		
-		Thread.sleep(10000);
-		System.out.println("Driver is executed successfully");
+		Set<String> windId = dr.getWindowHandles();
+		List<String> wind = new ArrayList<String>(windId);
+		String c1 = wind.get(0);
+		String c2 = wind.get(1);
+		System.out.println("Wind ID1: "+c1);
+		System.out.println("Wind ID1: "+c2);
+		
+		dr.switchTo().window(c2);
+		System.out.println(dr.getTitle());
+		Thread.sleep(5000);
 		dr.quit();
 		
 		
